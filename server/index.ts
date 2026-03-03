059c52e54081c2b92bd0d3cf600c76694affa6e3
Anshu <ansh448555@gmail.com>
2026-03-03 16:58:44 +0530

    fix CORS for App Runner

--- server/index.ts
+++ server/index.ts
@@ -17,6 +17,7 @@ const PORT = process.env.PORT ?? 3001
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:4173',
+  'https://thinkmate-self.vercel.app',
   process.env.FRONTEND_URL,
 ].filter(Boolean) as string[]
 
